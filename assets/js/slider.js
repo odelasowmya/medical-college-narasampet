637aaf14b5d56f91cceb4d50e6c607b671095aba
odela sowmya <sowmyaodela1998@gmail.com>
2024-04-16 18:24:23 +0530

    chages email

---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+var Swipes = new Swiper('.swiper-container', {
+  loop: true,
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+      el: '.swiper-pagination',
+  },
+});
